eae0f88700979bdc67b2493b3b532a4fd04e7e0a
dymirtaylorparish-max <dymirtaylorparish@gmail.com>
2026-03-22 00:01:59 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -19,6 +20,7 @@ app.get("/api/futures", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
